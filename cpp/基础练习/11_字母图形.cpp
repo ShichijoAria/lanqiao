8db5637c5489f3b1752ab4148feb20d61f69c68c
shichijoaria <15608176098@163.com>
2018-03-02 16:35:44 +0800

    update

--- cpp/基础练习/11_字母图形.cpp
+++ cpp/基础练习/11_字母图形.cpp
@@ -28,3 +28,27 @@ DCBABCD
         EDCBABC
 数据规模与约定
 1 <= n, m <= 26。*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n,m;
+    char letter='A';
+    cin>> n>>m;
+    for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < m; ++i) {
+            //
+            if(i>=j)
+                cout<<(char)(letter+i-(j%m));
+            else
+                cout<<(char)(letter+i+(m-(j%m)));
+
+            //cout << char('A' + abs(i - j));
+
+
+        }
+        cout<<endl;
+    }
+    return 0;
+}
